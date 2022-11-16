feb0a5fed8d615619dc4d069616df979fa1f061e
Abhishek Thapa <52848973+Abhishek004-thapa@users.noreply.github.com>
2022-11-16 09:02:49 +0545

    Add files via upload

---
+++ main_site/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import SurveyData,LRModel
+admin.site.register(SurveyData)
+admin.site.register(LRModel)
---
+++ main_site/views.py
@@ -0,0 +1,193 @@
+import json, csv
+import os
+
+from django.http import HttpResponseRedirect, JsonResponse
+
+from MyHair.settings import MEDIA_ROOT
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import SurveyData
+from .generate_csv import streaming_csv_view
+from . import train, predict
+
+
+def test(request):
+    if request.method == 'GET':
+        train.LR_train_manual()
+        return render(request, 'main_site/test.html', {})
+    else:
+        return HttpResponse('Forbidden')
+
+
+@login_required(login_url='login')
+def Home(request):
+    if request.method == 'GET':
+        return render(request, 'main_site/home.html', {})
+    else:
+        return HttpResponse('Forbidden')
+
+
+@login_required(login_url='login')
+def adminSite(request):
+    if request.method == 'GET':
+        return render(request, 'main_site/admin_site.html', {})
+    else:
+        return HttpResponse('Forbidden')
+
+
+def userHome(request):
+    if request.method == 'GET':
+        return render(request, 'main_site/user_home.html', {})
+    else:
+        return HttpResponse('Forbidden')
+
+
+def userSurvey(request):
+    if request.method == 'GET':
+        return render(request, 'main_site/user_survey.html', {})
+    else:
+        return HttpResponse('Forbidden')
+
+
+def predictMyFall(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.POST['survey'])
+            # print(len(data), data)
+            # print(data,type(data))
+            # data_loc = os.path.join(MEDIA_ROOT, 'survey_data')
+            # data_loc=data_loc+'/'+'hair_fall_dataset.csv'
+            # with open(data_loc, 'a',newline='') as f:
+            #     writer = csv.writer(f)
+            #     writer.writerow(data)
+            data = list(map(int, data))
+            per = predict.LR_predict_onfly([data[:22]])
+            return JsonResponse({
+                "message": "predicted",
+                "percent": str(per)
+            }, safe=False)
+        except:
+            return JsonResponse({
+                "message": "error",
+                "percent": ''
+            }, safe=False)
+    else:
+        return HttpResponse('Forbidden')
+
+
+@login_required(login_url='login')
+def drHome(request):
+    if request.method == 'GET':
+        return render(request, 'main_site/dr_survey.html', {})
+    else:
+        return HttpResponse('Forbidden')
+
+
+@login_required(login_url='login')
+def myContrib(request):
+    if request.method == 'GET':
+        return render(request, 'main_site/contrib.html', {})
+    else:
+        return HttpResponse('Forbidden')
+
+
+def Login(request):
+    if request.method == 'GET':
+        return render(request, 'main_site/login.html', {})
+    elif request.method == 'POST':
+        username = request.POST.get('lic_num')
+        password = request.POST.get('password')
+        next_url = request.GET.get('next')
+        user = authenticate(username=username, password=password)
+        # print('next',next_url)
+        if user is not None:
+            login(request, user)
+            if next_url:
+                return HttpResponseRedirect(next_url)
+            return redirect('home')
+        else:
+            messages.error(request, f'Incorrect Licence number or password!')
+            return render(request, 'main_site/login.html')
+    else:
+        return HttpResponse('Forbidden')
+
+
+@login_required(login_url='login')
+def saveSurvey(request):
+    if request.method == 'POST':
+        try:
+            # data = json.loads(request.POST.get('survey'))    this will give csrf token error after deployment so do like below
+
+            data = json.loads(request.POST['survey'])
+
+            # print(data,type(data))
+            data_loc = os.path.join(MEDIA_ROOT, 'survey_data')
+            data_loc = data_loc + '/' + 'hair_fall_dataset.csv'
+            with open(data_loc, 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow(data)
+            return HttpResponse("Saved")
+        except:
+            return HttpResponse("Error")
+    else:
+        return HttpResponse('Forbidden')
+
+
+@login_required(login_url='login')
+def saveSurveyDB(request):
+    if request.method == 'POST':
+        try:
+            # data = json.loads(request.POST.get('survey'))    this will give csrf token error after deployment so do like below
+            data = json.loads(request.POST['survey'])
+            # print(data,type(data))
+            tmp = SurveyData(Gender=data[0],
+                             Age=data[1],
+                             Marital_Status=data[2],
+                             Father=data[3],
+                             Mother=data[4],
+                             Grand_Father=data[5],
+                             Grand_Mother=data[6],
+                             Siblings=data[7],
+                             Pregnant=data[8],
+                             Radiation=data[9],
+                             Hairline_Pattern=data[10],
+                             Hairstyle=data[11],
+                             Density=data[12],
+                             Hair_Fall_Rate=data[13],
+                             Scalp_Infection=data[14],
+                             Pain_Itch=data[15],
+                             Nutrition=data[16],
+                             Weight_Loss=data[17],
+                             Sleeping_Pattern=data[18],
+                             Chemical_Products=data[19],
+                             Medication=data[20],
+                             Region=data[21],
+                             Label=data[22]
+                             )
+            tmp.save()
+            SurveyData.refresh_from_db(tmp)
+            # print(data[:22],len(data[:22]))
+            # print(data[22])
+            # train.LR_train_onFly([data[:22]], [data[22]])
+            return HttpResponse("Saved")
+        except:
+            return HttpResponse("Error")
+
+    else:
+        return HttpResponse('Forbidden')
+
+
+@login_required(login_url='login')
+def getCSV(request):
+    return streaming_csv_view(request)
+
+
+def userResult(request, per):
+    if request.method == 'GET':
+        print(per)
+        return render(request, 'main_site/user_result.html', {'per': float(per)})
+    else:
+        return HttpResponse('Forbidden')
